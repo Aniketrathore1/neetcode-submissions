e1d7a011f1aec3e8e8cb165b12a7783f2a6aee87
Aniket <125176109+Aniketrathore1@users.noreply.github.com>
2026-07-13 22:50:34 +0530

    Add: unique-paths-ii - submission-0

---
+++ Data Structures & Algorithms/unique-paths-ii/submission-0.java
@@ -0,0 +1,35 @@
+class Solution {
+    int[][] memo;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        memo = new int[m][n];
+
+
+        for(int i = 0 ; i < m ; i++){
+            Arrays.fill(memo[i] , -1);
+        }
+
+        return solve(0 , 0 , obstacleGrid);
+    }
+
+    private int solve(int i  , int j  , int[][] grid){
+        int m = grid.length;
+        int n = grid[0].length;
+
+        if(i >= m || j >= n){
+            return 0;
+        }
+
+        if(grid[i][j] == 1) return 0;
+        if(i == m-1 && j == n-1) return 1;
+        
+
+        if(memo[i][j] != -1) return memo[i][j];
+
+        int down = solve(i+1 , j , grid);
+        int right = solve(i , j+1 , grid);
+
+        return memo[i][j] = down+right;
+    }
+}
