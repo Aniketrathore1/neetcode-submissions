32b0e93527d882e331ecb875742ec95aada13065
Aniket <125176109+Aniketrathore1@users.noreply.github.com>
2026-07-28 18:19:07 +0530

    Add: car-pooling - submission-1

---
+++ Data Structures & Algorithms/car-pooling/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean carPooling(int[][] trips, int capacity) {
+        int[] diff = new int[1001];
+
+        for(int[] trip : trips){
+            int passenger = trip[0];
+            int from = trip[1];
+            int to = trip[2];
+
+            diff[from] += passenger;
+            diff[to] -= passenger;
+        }
+
+        int curr = 0;
+        for(int i = 0 ; i<=1000 ; i++){
+            curr += diff[i];
+            if(curr > capacity) return false;
+        }
+        return true;
+    }
+}
